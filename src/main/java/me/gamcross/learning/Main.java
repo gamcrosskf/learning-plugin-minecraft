c728851448945901d2452b4d72daef63bddd2c14
Kenzo Farrugia <gamcrosskf@gmail.com>
2022-12-09 15:21:22 +0100

    Steup server & and plugin

---
+++ src/main/java/me/gamcross/learning/Main.java
@@ -0,0 +1,10 @@
+package me.gamcross.learning;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        System.out.println("Learning is enable!");
+    }
+}
