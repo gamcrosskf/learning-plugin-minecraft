ada27aee081d588bc473bcf83c061723190bc45a
Kenzo Farrugia <gamcrosskf@gmail.com>
2022-12-10 14:39:16 +0100

    Add Command Test and Config.yml

--- src/main/java/me/gamcross/learning/Main.java
+++ src/main/java/me/gamcross/learning/Main.java
@@ -1,20 +1,26 @@
 package me.gamcross.learning;
 
 import me.gamcross.learning.commands.CommandBroadcast;
+import me.gamcross.learning.commands.CommandTestEnable;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-    public static String prefix = "§7[§6Learning§7]§r ";
+    public static String prefix = "§7§l[§6§lLearning§7§l]§r "; // Prefix for all messages
 
     @Override
     public void onEnable() {
+        // Plugin startup logic
         System.out.println("Learning is enable!");
+        saveDefaultConfig(); // Save default config.yml
 
+        // Register commands
+        getCommand("testLearning").setExecutor(new CommandTestEnable(this));
         getCommand("broadcast").setExecutor(new CommandBroadcast());
     }
 
     @Override
     public void onDisable() {
+        // Plugin shutdown logic
         System.out.println("Learning is disable!");
     }
 }
---
+++ src/main/java/me/gamcross/learning/commands/CommandTestEnable.java
@@ -0,0 +1,25 @@
+package me.gamcross.learning.commands;
+
+import me.gamcross.learning.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandTestEnable implements CommandExecutor {
+    private Main main; // Main class
+    public CommandTestEnable(Main main) {
+        this.main = main;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if(commandSender.hasPermission("learning.test")) {
+            commandSender.sendMessage(main.prefix + main.getConfig().getString("commands.testLearning.color") + main.getConfig().getString("commands.testLearning.message"));
+        } else {
+            commandSender.sendMessage(main.prefix + ChatColor.RED + "You don't have permission to use this command!");
+        }
+        return false;
+    }
+}
